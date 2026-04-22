b395bfbf32b84e5555654e027aca4e5ac3461ede
Jonathas Campos Pimenta <dontemaster@outlook.com>
2026-04-22 07:59:43 -0300

    Padrao TOS json

--- src/test/java/TestTOS.java
+++ src/test/java/TestTOS.java
@@ -11,13 +11,15 @@ import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestTOS {
-	private final static String host = "http://172.10.2.25/tosp";
+	private final static String host = "https://tos-homolog.portodoitaqui.com/tosp";
 	private final static String user = "externo";
-	private final static String pass = "*******";
+	private final static String pass = "******";
 	private String sessionId;
 	
 	public TestTOS() {
@@ -50,6 +52,30 @@ public class TestTOS {
 		}
 	}
 	
+	private String getJsonConsulta(String entityName, JSONObject object) {
+		JSONArray item = new JSONArray();
+		item.put("java.util.HashMap");
+		item.put(object);
+		JSONObject type = new JSONObject();
+		type.put(entityName, item);
+		JSONArray root = new JSONArray();
+		root.put("tosp.foundation.core.kernel.coqueryobject.CoQueryObjectDynamic");
+		root.put(type);
+		return root.toString();
+	}
+	
+	private String getJsonCoQueryObject(String fullPathDTO, JSONObject object) {
+		JSONArray item = new JSONArray();
+		item.put(fullPathDTO);
+		item.put(object);
+		JSONArray innerArray = new JSONArray();
+		innerArray.put(item);
+		JSONArray root = new JSONArray();
+		root.put("tosp.foundation.core.kernel.coqueryobject.CoQueryObject");
+		root.put(innerArray);
+		return root.toString();
+	}
+	
 	public String getSessionId() {
 		return sessionId;
 	}
@@ -73,51 +99,102 @@ public class TestTOS {
 			Assert.assertNotNull("Não foi possivel fazer o login", getSessionId());
 			return;
 		}
-		String json = "[\"tosp.foundation.core.kernel.coqueryobject.CoQueryObjectDynamic\"," + 
-				"        {\"Empresa\":[\"java.util.HashMap\",	" + 
-				"	   	   	  {" + 
-				"	   	   	   	  \"tipo\":2," + 
-				"	   	   	   	  \"page\":0" + 
-				"	   	   	  }" + 
-				"	 ]}]";
-		String result = executarPost(getSessionId(), "ManutenirEmpresaEmap/filtrar", json);
+		JSONObject object = new JSONObject();
+		object.put("nome", "STARMAR");
+		object.put("tipo", 2);
+		object.put("page", 0);
+		String result = executarPost(getSessionId(), "ManutenirEmpresaEmap/filtrar", getJsonConsulta("Empresa", object));
 		System.out.println(result);
 		Assert.assertFalse(result.toLowerCase().contains("bad credentials"));	
 	}
 	
-	@Test
+	//@Test
 	public void testManutenirOperacaoNavio() {
 		if(getSessionId() == null) {
 			Assert.assertNotNull("Não foi possivel fazer o login", getSessionId());
 			return;
 		}
-		String json = "[\"tosp.foundation.core.kernel.coqueryobject.CoQueryObjectDynamic\"," + 
-				"        {\"OperacaoNavio\":[\"java.util.HashMap\",	" + 
-				"	   	   	  {" + 
-				"	   	   	   	  \"viagemId\":1499905 ," + 
-				"	   	   	   	  \"page\":0" + 
-				"	   	   	  }" + 
-				"	 ]}]";
-		String result = executarPost(getSessionId(), "ManutenirOperacaoNavio/filtrar", json);
+		JSONObject object = new JSONObject();
+		object.put("viagemId", 1499905);
+		object.put("page", 0);
+		String result = executarPost(getSessionId(), "ManutenirOperacaoNavio/filtrar", getJsonConsulta("OperacaoNavio", object));
 		System.out.println(result);
 		Assert.assertFalse(result.toLowerCase().contains("bad credentials"));
 	}
 	
-	@Test
+	//@Test
 	public void testManutenirPerfil() {
 		if(getSessionId() == null) {
 			Assert.assertNotNull("Não foi possivel fazer o login", getSessionId());
 			return;
 		}
-		String json = "[\"tosp.foundation.core.kernel.coqueryobject.CoQueryObjectDynamic\"," + 
-				"        {\"Perfil\":[\"java.util.HashMap\",	" + 
-				"	   	   	  {" + 
-				"	   	   	   	  \"page\":0" + 
-				"	   	   	  }" + 
-				"	 ]}]";
-		String result = executarPost(getSessionId(), "ManutenirPerfil/filtrar", json);
+		JSONObject object = new JSONObject();
+		String result = executarPost(getSessionId(), "ManutenirPerfil/filtrar", getJsonConsulta("Perfil", object));
 		System.out.println(result);
 		Assert.assertFalse(result.toLowerCase().contains("bad credentials"));
 	}
 	
+	//@Test
+	public void testManutenirEmpresaIncluir() {
+		if(getSessionId() == null) {
+			Assert.assertNotNull("Não foi possivel fazer o login", getSessionId());
+			return;
+		}
+		String fullPath = "tosp.plugin.comun.generic.empresa.model.EmpresaDTO";
+		JSONObject empresa = new JSONObject();
+		empresa.put("nome", "JUREMACENTER");
+		empresa.put("cnpj", "03.743.193/0001-69");
+		empresa.put("email", "JUREMACENTER@GMAIL.COM");
+		empresa.put("kdPratico", false);
+		empresa.put("kdRebocador", true);
+		empresa.put("kdAmarracao", false);
+
+		String json = getJsonCoQueryObject(fullPath, empresa);		
+		String result = executarPost(getSessionId(), "ManutenirEmpresaEmap/incluir", json);
+		System.out.println(result);
+		Assert.assertFalse(result.toLowerCase().contains("bad credentials"));
+	}
+	
+	public void testManutenirEmpresaAlterar() {
+		if(getSessionId() == null) {
+			Assert.assertNotNull("Não foi possivel fazer o login", getSessionId());
+			return;
+		}
+		String fullPath = "tosp.plugin.comun.generic.empresa.model.EmpresaDTO";
+		JSONObject empresa = new JSONObject();
+		empresa.put("id", 1000L);
+		empresa.put("nome", "XXXXX");
+		empresa.put("cnpj", "03.743.193/0001-69");
+		empresa.put("email", "qualquer@email.com");
+		
+		String json = getJsonCoQueryObject(fullPath, empresa);		
+		String result = executarPost(getSessionId(), "ManutenirEmpresaEmap/alterar", json);
+		System.out.println(result);
+		Assert.assertFalse(result.toLowerCase().contains("bad credentials"));
+	}
+	
+	@Test
+	public void testManutenirEmpresaDeletar() {
+		if(getSessionId() == null) {
+			Assert.assertNotNull("Não foi possivel fazer o login", getSessionId());
+			return;
+		}
+		String fullPath = "tosp.plugin.comun.generic.empresa.model.EmpresaDTO";
+		JSONObject empresa = new JSONObject();
+		empresa.put("id", 10446L);
+		empresa.put("nome", "XXXXX");
+		empresa.put("cnpj", "03.743.193/0001-69");
+		empresa.put("email", "qualquer@email.com");
+		String json = getJsonCoQueryObject(fullPath, empresa);	
+		String result = executarPost(getSessionId(), "ManutenirEmpresaEmap/delete", json);
+		System.out.println(result);
+		Assert.assertFalse(result.toLowerCase().contains("bad credentials"));
+	}
 }
+/* String json = "[\"tosp.foundation.core.kernel.coqueryobject.CoQueryObjectDynamic\"," + 
+		"        {\"Perfil\":[\"java.util.HashMap\",	" + 
+		"	   	   	  {" + 
+		"	   	   	   	  \"page\":0" + 
+		"	   	   	  }" + 
+		"	 ]}]";
+*/
